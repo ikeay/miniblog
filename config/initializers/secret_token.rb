325e390c3bc27675d087cc58faf8b8d7dd07f415
Ayaka Ikezawa <ikezawa.0728@gmail.com>
2012-02-03 14:01:34 +0900

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miniblog::Application.config.secret_token = 'f01dd02cb4e97703767f668a8dd25c403159a6bcc3ebccdc7177a0a6f8d136c6579ebe442ce360c02e4b8984be6b79f817e36b705020323ffd18f1c97169e30d'
